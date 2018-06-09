99d9cfa6b795a9049cbee732b4f8f6f2fbe66dae
jlga94 <jose.gil@pucp.pe>
2018-06-09 17:34:50 -0500

    Add columns in Json File and create CSV file

--- zuluScraper.py
+++ zuluScraper.py
@@ -7,91 +7,239 @@ from selenium.webdriver.support import expected_conditions as EC
 from contextlib import contextmanager
 from selenium.webdriver.support.expected_conditions import staleness_of
 import selenium
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
 import re, time, datetime
 from bs4 import BeautifulSoup
-import csv, string, sys
+import csv, string, sys, json
 
 from time import sleep
+import datetime
 import pandas as pd
+import os, sys
+import shutil,glob
 
-columns = ['TraderName','Pips','Operaciones','Roi','MediaPips','GananciaPorc','MaxDDPips','Semanas','Usuarios','CantidadSeguida','BeneficioSegCuentaReal']
+class LoginException(Exception):
+	def __init__(self):
+		Exception.__init__(self,"Usuario o Password incorrectos")
 
-
-
-
-def getDataPerTrader(html_source):
-	soup = BeautifulSoup(html_source, 'lxml')
-
-	nameElement = soup.find("a", {"class": "text-main zlds-link--brand"})
-
-
-
-browser = None
-
-urlLogin = "https://es.zulutrade.com/login"
-urlToScrap = "https://es.zulutrade.com/traders"
-
-user = "jose.gil@pucp.pe"
-password = "recoleta11"
+def getColumns(columnsFile):
+	with open(columnsFile, "r") as f:
+		return json.load(f)
 
 
-profile = webdriver.FirefoxProfile()
-profile.set_preference("dom.disable_beforeunload", True)
-
-profile.set_preference("browser.tabs.remote.autostart", False)
-profile.set_preference("browser.tabs.remote.autostart.1", False)
-profile.set_preference("browser.tabs.remote.autostart.2", False)
-
-profile.set_preference("browser.tabs.remote.force-enable", False)
-
-driver = webdriver.Firefox(firefox_profile = profile)
-
-driver.get(urlLogin)
-
-userElement = driver.find_element_by_id("main_tbUsername")
-passwordElement = driver.find_element_by_id("main_tbPassword")
-
-userElement.send_keys(user)
-passwordElement.send_keys(password)
+def createTodayDirectory(todayDirectory):
+	if os.path.exists(todayDirectory):
+		shutil.rmtree(todayDirectory)
+	os.makedirs(todayDirectory)
 
 
-driver.find_element_by_id("main_btnLogin").click()
+def getLastFilename(path):
+	list_of_files = glob.glob(path + '/*')
+	latest_file = max(list_of_files, key=os.path.getctime)
+	return latest_file
 
 
-sleep(4)
+def getDataPerTrader(rowElement,ubications):
+	data = {}
+	for ubication in ubications.keys():
+		element = rowElement.find_element_by_xpath(ubications[ubication]["XPATH"])
+		if "Attribute" in ubications[ubication].keys():
+			data[ubication] = element.get_attribute(ubications[ubication]["Attribute"])
+		else:
+			element = element.find_element_by_xpath('..')
+			data[ubication] = element.text.split('\n')[1]
 
-driver.get(urlToScrap)
-delay = 25 #seconds
+	print(data)
+	return data
 
-'''
-try:
-	myElem = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.CLASS_NAME, 'zl-performance-table')))
-except TimeoutException:
-	print("Se excedió el tiempo de espera")
+def writeHeaderFile(outputFile,columns):
+	dfTraders = pd.DataFrame(columns=columns)
+
+	#Write CSV file header
+	with open(outputFile, "w") as f:
+		dfTraders.to_csv(f, header=True, index=False,encoding='ISO-8859-1', sep='|')
+
+def main(user,password):
+	urlRoot = "https://es.zulutrade.com"
+	urlLogin = "https://es.zulutrade.com/login"
+	urlToScrap = "https://es.zulutrade.com/traders"
+
+	columnsFile = "ubicationColumns.json"
+	firefoxDirectory = r'D:\Navegadores\Mozilla Firefox\firefox.exe'
+
+	today = datetime.datetime.strftime(datetime.datetime.now(), '%Y_%m_%d')
+	createTodayDirectory(today)
+
+	outputFile = "zulutrade_" + today + ".csv"
+
+	columnsJson = getColumns(columnsFile)
+
+	writeHeaderFile(outputFile,columnsJson["Columns"])
+
+	profile = webdriver.FirefoxProfile()
+	profile.set_preference("dom.disable_beforeunload", True)
+
+	profile.set_preference("browser.tabs.remote.autostart", False)
+	profile.set_preference("browser.tabs.remote.autostart.1", False)
+	profile.set_preference("browser.tabs.remote.autostart.2", False)
+	profile.set_preference("browser.tabs.remote.force-enable", False)
+
+	profile.set_preference('browser.download.folderList', 2) # custom location
+	profile.set_preference('browser.download.manager.showWhenStarting', False)
+	profile.set_preference('browser.download.dir', os.getcwd()+ '\\' + today)
+	profile.set_preference('browser.helperApps.neverAsk.saveToDisk', "application/xml,text/xml,application/csv,application/excel,application/vnd.msexcel,application/vnd.ms-excel,text/anytext,text/comma-separated-values,text/csv,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/octet-stream")
+	profile.set_preference("browser.helperApps.neverAsk.openFile","application/xml,text/xml,application/csv,application/excel,application/vnd.msexcel,application/vnd.ms-excel,text/anytext,text/comma-separated-values,text/csv,application/vnd.ms-excel,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/octet-stream")
+	profile.set_preference("browser.helperApps.alwaysAsk.force", False)
+	profile.set_preference("browser.download.manager.useWindow", False)
+	profile.set_preference("browser.download.manager.focusWhenStarting", False)
+	profile.set_preference("browser.download.manager.alertOnEXEOpen", False)
+	profile.set_preference("browser.download.manager.showAlertOnComplete", False)
+	profile.set_preference("browser.download.manager.closeWhenDone", True)
+
+	binary = FirefoxBinary(firefoxDirectory)
+
+	driver = webdriver.Firefox(firefox_profile = profile,firefox_binary=binary)
+	#driver = webdriver.Firefox(firefox_profile = profile)
+
+	driver.get(urlLogin)
+
+	userElement = driver.find_element_by_id("main_tbUsername")
+	passwordElement = driver.find_element_by_id("main_tbPassword")
+
+	userElement.send_keys(user)
+	passwordElement.send_keys(password)
+
+
+	driver.find_element_by_id("main_btnLogin").click()
+
+	delayLogin = 10 #seconds
+	delay = 25 #seconds
+
+	try:
+		element = WebDriverWait(driver, delayLogin).until(EC.presence_of_element_located((By.ID,'user-top-container')))
+	except TimeoutException:
+		print("Se excedió el tiempo de espera")
+		driver.quit()
+		raise LoginException()
+
+	driver.get(urlToScrap)
+	
+	try:
+		element = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,'//zl-load-more/button')))
+	except TimeoutException:
+		print("Se excedió el tiempo de espera")
+		driver.quit()
+		raise Exception()
+
+	moreDetailElement = driver.find_elements_by_xpath("//zl-performance/div/div/div/div/button")
+	print(len(moreDetailElement))
+
+	moreDetailElement[0].click()
+
+
+	for i in range(1):
+		downloadMoreElement = driver.find_element_by_xpath("//zl-load-more/button")
+		downloadMoreElement.click()
+
+		sleep(4.5)
+
+
+	rowsElements = driver.find_elements_by_xpath("//zl-performance-forex-list/div/table/tbody")
+	print(len(rowsElements))
+
+	badgesElements = driver.find_elements_by_xpath("//zl-trader-badge")
+	print(len(badgesElements))
+
+	for iRowElement in range(len(rowsElements)):
+		print(iRowElement)
+		rowData = getDataPerTrader(rowsElements[iRowElement],columnsJson["Ubications"])
+
+		numElements = len(badgesElements[iRowElement].find_elements_by_xpath(".//ngl-icon[@ng-reflect-set-icon='icon-badge-partially-verified' or @ng-reflect-set-icon='icon-badge-fully-verified']"))
+		print(numElements)
+
+		if numElements > 0:
+			print("Si hay elemento Check")
+			checkIconElement = badgesElements[iRowElement].find_elements_by_xpath(".//ngl-icon[@ng-reflect-set-icon='icon-badge-partially-verified' or @ng-reflect-set-icon='icon-badge-fully-verified']")[numElements - 1]
+			
+			driver.execute_script("arguments[0].scrollIntoView();", rowsElements[iRowElement])
+			
+			hover = ActionChains(driver).move_to_element(checkIconElement)
+			hover.perform()
+
+			sleep(2)
+
+			soup = BeautifulSoup(driver.page_source, 'lxml')
+			popUpElement = soup.find("zl-trader-verification-popover")
+			#print(popUpElement)
+
+			#To get lost of Focus of the little windows to iterate the next row
+			hover = ActionChains(driver).move_to_element(badgesElements[iRowElement])
+			hover.perform()
+			sleep(1)
+
+		#open tab
+		driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
+
+		print(driver.window_handles)
+
+
+		driver.switch_to.window(driver.window_handles[1])
+
+		driver.get(rowData["Url"])
+
+		try:
+			element = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH,'//zl-timeframes/ngl-picklist/div/button')))
+		except TimeoutException:
+			print("Se excedió el tiempo de espera")
+			driver.quit()
+			raise Exception()
+
+		graphicTimeElement = driver.find_element_by_xpath("//zl-timeframes/ngl-picklist/div/button")
+		graphicTimeElement.click()
+
+		graphicTotalTimeElements = driver.find_elements_by_xpath("//zl-timeframes/ngl-picklist/div/div/ul/li")
+		graphicTotalTimeElements[len(graphicTotalTimeElements) - 1].click()
+
+
+		exportExcelElement = driver.find_element_by_xpath("//zl-trading-history-excel-export/span/button")
+		exportExcelElement.click()
+
+		exportExcel2007Elements = driver.find_elements_by_xpath("//zl-trading-history-excel-export/span/div/ul/li")
+		exportExcel2007Elements[0].click()
+
+		sleep(3)
+
+		excelFilename = getLastFilename(os.getcwd() + '\\' + today)
+
+		rowData["Excel"] = excelFilename
+
+		print(rowData)
+
+		dfTraders = pd.DataFrame(rowData,columns=columnsJson["Columns"], index=[0])
+		with open(outputFile, "a") as f:
+			dfTraders.to_csv(f, header=None, index=False,encoding='ISO-8859-1', sep='|')
+
+		# close the tab
+		driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 'w') 
+
+
+		driver.switch_to.window(driver.window_handles[0])
+
 	driver.quit()
-'''
-
-sleep(10)
-
-moreDetailElement = driver.find_elements_by_xpath("//button[@nglbutton='neutral']")
-print(len(moreDetailElement))
-
-moreDetailElement[0].click()
 
 
-for i in range(9):
-
-	downloadMoreElement = driver.find_element_by_xpath("//button[@class='slds-button slds-button--inverse']")
-	downloadMoreElement.click()
-
-	sleep(4.5)
-
-
-
-
-rowsElements = driver.find_elements_by_xpath("//tbody[@class='large']")
-
-for rowElement in rowsElements:
-
-	rowData = getDataPerTrader(rowElement.get_attribute('innerHTML'))
+if __name__ == "__main__":
+	try:
+		user = sys.argv[1]
+		password = sys.argv[2]
+		print("User: "+ user)
+		print("Password: "+ password)
+		main(user,password)
+	except LoginException:
+		print("Usuario o Password Incorrectos")
+	#except:
+	#	print("Unexpected error:", sys.exc_info())
+	
